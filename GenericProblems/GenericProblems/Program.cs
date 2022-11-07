8680b2c006013140e89d7df1d42f90a1eec85aff
Piyushnmj <piyushnmj@yahoo.com>
2022-11-07 19:10:32 +0530

    Max String at 1st Position

--- GenericProblems/GenericProblems/Program.cs
+++ GenericProblems/GenericProblems/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome to Generic Based Problems");
 
-            string output = MaxNumberCheck.MaxIntegerNumber("Apple", "Peach", "Banana");
+            string output = MaxNumberCheck.MaxIntegerNumber("Peach", "Apple", "Banana");
             Console.WriteLine("\nMax string word is: " + output);
         }
     }
