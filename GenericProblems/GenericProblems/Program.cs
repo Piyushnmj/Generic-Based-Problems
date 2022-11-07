21b2c010dc4ebd2dbce01a2f59ce0613a1f1be8e
Piyushnmj <piyushnmj@yahoo.com>
2022-11-07 19:30:05 +0530

    Using 4 Parameters

--- GenericProblems/GenericProblems/Program.cs
+++ GenericProblems/GenericProblems/Program.cs
@@ -6,13 +6,13 @@
         {
             Console.WriteLine("Welcome to Generics Problems");
 
-            MaxNumberCheck<int> objint = new MaxNumberCheck<int>(11, 111, 1111);
+            MaxNumberCheck<int> objint = new MaxNumberCheck<int>(11, 111, 1111, 11111);
             Console.WriteLine("\nMax integer number is: " + objint.MaxMethod());
 
-            MaxNumberCheck<float> objfloat = new MaxNumberCheck<float>(1, 11, 111);
+            MaxNumberCheck<float> objfloat = new MaxNumberCheck<float>(1, 11, 111, 1111);
             Console.WriteLine("\nMax float number is: " + objfloat.MaxMethod());
 
-            MaxNumberCheck<string> objstring = new MaxNumberCheck<string>("Apple", "Peach", "Banana");
+            MaxNumberCheck<string> objstring = new MaxNumberCheck<string>("Apple", "Peach", "Banana", "Orange");
             Console.WriteLine("\nMax string word is: " + objstring.MaxMethod());
         }
     }
