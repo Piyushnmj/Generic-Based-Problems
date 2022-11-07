2e91de99ae9359e95568f96da96ad8142092c493
Piyushnmj <piyushnmj@yahoo.com>
2022-11-07 18:38:17 +0530

    Find the Max Integer

--- GenericProblems/GenericProblems/Program.cs
+++ GenericProblems/GenericProblems/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Generic Based Problems");
+
+            int output = MaxNumberCheck.MaxIntegerNumber(11, 111, 1111);
+            Console.WriteLine("\nMax integer number is: " + output);
         }
     }
 }
