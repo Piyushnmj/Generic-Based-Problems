0b00a90284581caa7edd9ebbee517a685cd7a6c9
Piyushnmj <piyushnmj@yahoo.com>
2022-11-07 18:33:32 +0530

    Welcome

---
+++ GenericProblems/GenericProblems/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GenericProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generic Based Problems");
+        }
+    }
+}
