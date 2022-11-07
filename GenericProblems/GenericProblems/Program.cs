c867462bbd1a3a855b6a261cd984a3a31a14dbe3
Piyushnmj <piyushnmj@yahoo.com>
2022-11-07 19:22:49 +0530

    Refactor using Generic Class

--- GenericProblems/GenericProblems/Program.cs
+++ GenericProblems/GenericProblems/Program.cs
@@ -4,18 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Generic Based Problems");
+            Console.WriteLine("Welcome to Generics Problems");
 
-            MaxNumberCheck objMaxNumber = new MaxNumberCheck();
+            MaxNumberCheck<int> objint = new MaxNumberCheck<int>(11, 111, 1111);
+            Console.WriteLine("\nMax integer number is: " + objint.MaxMethod());
 
-            int output1 = objMaxNumber.MaxNumber<int>(11, 111, 1111);
-            Console.WriteLine("\nMax integer number is: " + output1);
+            MaxNumberCheck<float> objfloat = new MaxNumberCheck<float>(1, 11, 111);
+            Console.WriteLine("\nMax float number is: " + objfloat.MaxMethod());
 
-            float output2 = objMaxNumber.MaxNumber<float>(1, 11, 111);
-            Console.WriteLine("\nMax float number is: " + output2);
-
-            string output3 = objMaxNumber.MaxNumber<string>("Apple", "Peach", "Banana");
-            Console.WriteLine("\nMax string word is: " + output3);
+            MaxNumberCheck<string> objstring = new MaxNumberCheck<string>("Apple", "Peach", "Banana");
+            Console.WriteLine("\nMax string word is: " + objstring.MaxMethod());
         }
     }
 }
