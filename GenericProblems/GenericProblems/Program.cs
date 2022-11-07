32bc94b75a395a79a32ffe4dae0d30ac3fcc7848
Piyushnmj <piyushnmj@yahoo.com>
2022-11-07 18:43:08 +0530

    Max number at 1st Position

--- GenericProblems/GenericProblems/Program.cs
+++ GenericProblems/GenericProblems/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome to Generic Based Problems");
 
-            int output = MaxNumberCheck.MaxIntegerNumber(11, 111, 1111);
+            int output = MaxNumberCheck.MaxIntegerNumber(1111, 111, 11);
             Console.WriteLine("\nMax integer number is: " + output);
         }
     }
