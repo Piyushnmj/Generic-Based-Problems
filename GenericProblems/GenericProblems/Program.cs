f0c64c3015ce52b785a9ca0e0d0fb3e9453ffa05
Piyushnmj <piyushnmj@yahoo.com>
2022-11-07 19:06:56 +0530

    Find Max String Word

--- GenericProblems/GenericProblems/Program.cs
+++ GenericProblems/GenericProblems/Program.cs
@@ -6,8 +6,8 @@
         {
             Console.WriteLine("Welcome to Generic Based Problems");
 
-            float output = MaxNumberCheck.MaxIntegerNumber(1, 11, 111);
-            Console.WriteLine("\nMax float number is: " + output);
+            string output = MaxNumberCheck.MaxIntegerNumber("Apple", "Peach", "Banana");
+            Console.WriteLine("\nMax string word is: " + output);
         }
     }
 }
