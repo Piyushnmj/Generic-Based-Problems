0a596cc30d7627e161afcdb21ab5e60d75ab984b
Piyushnmj <piyushnmj@yahoo.com>
2022-11-07 19:01:49 +0530

    Max Float at 3rd Position

--- GenericProblems/GenericProblems/Program.cs
+++ GenericProblems/GenericProblems/Program.cs
@@ -6,7 +6,7 @@
         {
             Console.WriteLine("Welcome to Generic Based Problems");
 
-            float output = MaxNumberCheck.MaxIntegerNumber(1, 111, 11);
+            float output = MaxNumberCheck.MaxIntegerNumber(1, 11, 111);
             Console.WriteLine("\nMax float number is: " + output);
         }
     }
